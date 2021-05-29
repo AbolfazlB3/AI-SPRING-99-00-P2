1c52ccd62f3bcb1e0a5e6e85f27cd6beb667f856
Abolfazl Bakiasay <abolfazl850@gmail.com>
2021-05-29 14:46:27 +0430

    Completed FIttness function (get_score)

--- src/Game.py
+++ src/Game.py
@@ -12,7 +12,6 @@ class Game:
             return False
         self.current_level_index += 1
         self.current_level_len = len(self.get_current_level()[1])
-        self.rewards = [None for i in range(self.current_level_len)]
         return True
 
     def get_current_level(self):
@@ -21,59 +20,69 @@ class Game:
         except:
             return None
 
-    def get_score(self, actions):
-        current_name, current_level = self.get_current_level()
-        if(current_level == None):
-            return 0
-        steps = 0
-        for i in range(self.current_level_len - 1):
-            current_step = current_level[i]
-            if (current_step == '_'):
-                steps += 1
-            elif (current_step == 'G' and actions[i - 1] == '1'):
-                steps += 1
-            elif (current_step == 'L' and actions[i - 1] == '2'):
-                steps += 1
-            else:
-                break
-        return steps == self.current_level_len - 1, steps
-
     WIN_REWARD = 4.0
     FLAG_REWARD = 1.0
     MUSHROOM_REWARD = 2.0
     KILL_REWARD = 2.0
     JUMP_REWARD = -0.4
+    MAX_STEP_REWARD = 0.2
+    DEATH_REWARD = -2.5
+
+    def get_score(self, actions):
+        cll = self.current_level_len
+        self.rewards = [None for i in range(cll)]
+        current_name, current_level = self.get_current_level()
+        if(current_level == None):
+            return None
+        res = self.get_score_rec(actions, current_level)
+
+        reward = res[3]
+        reward += res[1] * self.MAX_STEP_REWARD
+        reward += res[2] * self.DEATH_REWARD
+
+        return (res[2] == 0, )
 
     # (number of steps without dieing, max number of steps without dieing,
     #  number of deaths, total reward)
-    def get_score_rec(self, actions, level, ind, air=False):
+    def get_score_rec(self, actions, level, ind=0, air=False):
 
         if (self.rewards[ind] != None):
             return self.rewards[ind]
 
         cll = self.current_level_len
-        action = actions[ind]
+        act = actions[ind]
+        cell = level[ind+1]
 
         if(ind == cll-1):
             self.rewards[ind] = (
-                1, 1, 0, self.FLAG_REWARD if action == "1" else 0
-            )
+                1, 1, 0, self.FLAG_REWARD if act == "1" else 0)
             return self.rewards[ind]
 
         reward = 0.0
         lose = False
 
-        if():
-            pass
+        if ((cell == "G" and act != "1" and not air) or
+                (cell == "L" and act != "2")):
+            lose = True
 
-        for i in range(self.current_level_len - 1):
-            current_step = current_level[i]
-            if (current_step == '_'):
-                steps += 1
-            elif (current_step == 'G' and actions[i - 1] == '1'):
-                steps += 1
-            elif (current_step == 'L' and actions[i - 1] == '2'):
-                steps += 1
-            else:
-                break
-        return steps == self.current_level_len - 1, steps
+        if (cell == "G" and air):
+            reward += self.KILL_REWARD
+
+        if (cell == "M" and act != "1"):
+            reward += self.MUSHROOM_REWARD
+
+        if(act == "1"):
+            reward += self.JUMP_REWARD
+
+        cont = self.get_score_rec(actions, level, ind+1, act == "1")
+
+        steps = 0 if lose else cont[0] + 1
+
+        self.rewards[ind] = (
+            steps,
+            max(steps, cont[1]),
+            cont[2] + (1 if lose else 0),
+            cont[3] + reward,
+        )
+
+        return self.rewards[ind]
